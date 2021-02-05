cbc3953ac57d0c9699cbbee4d701900a5572a8a5
planethouki <26595148+planethouki@users.noreply.github.com>
2021-02-05 23:46:26 +0900

    sample code

---
+++ Assets/MyFirstUnityPackage/GraphicFadeOut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MyFirstUnityPackage
+{
+    public class GraphicFadeOut : MonoBehaviour
+    {
+        [SerializeField] private Graphic graphic = default;
+        [SerializeField] private float  duration = 1f;
+        [SerializeField] private float delay = 0f;
+        
+        public IEnumerator FadeOut()
+        {
+            yield return new WaitForSeconds(delay);
+            var elapsedTime = 0f;
+            while (elapsedTime < duration)
+            {
+                graphic.color = Color.Lerp(Color.white, Color.clear, elapsedTime / duration);
+                yield return null;
+                elapsedTime += Time.deltaTime;
+            }
+            
+            graphic.color = Color.clear;
+        }
+    }
+}
