3fe4e8d0006d9c0b06e26a2afc0c71a48d6a67f7
planethouki <26595148+planethouki@users.noreply.github.com>
2021-02-05 23:46:11 +0900

    PackageExporter.cs

---
+++ Assets/Editor/PackageExporter.cs
@@ -0,0 +1,38 @@
+﻿#if UNITY_EDITOR
+
+using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public static class PackageExporter
+    {
+        [MenuItem("Tools/Export Unitypackage")]
+        public static void Export()
+        {
+            // configure
+            var root = "MyFirstUnityPackage";
+            var exportPath = "./MyFirstUnityPackage.unitypackage";
+
+            var path = Path.Combine(Application.dataPath, root);
+            var assets = Directory.EnumerateFiles(path, "*", SearchOption.AllDirectories)
+                .Where(x => Path.GetExtension(x) == ".cs" || Path.GetExtension(x) == ".asmdef" || Path.GetExtension(x) == ".json" || Path.GetExtension(x) == ".unity" || Path.GetExtension(x) == ".meta")
+                .Select(x => "Assets" + x.Replace(Application.dataPath, "").Replace(@"\", "/"))
+                .ToArray();
+
+            Debug.Log("Export below files" + Environment.NewLine + string.Join(Environment.NewLine, assets));
+
+            AssetDatabase.ExportPackage(
+                assets,
+                exportPath,
+                ExportPackageOptions.Default);
+
+            Debug.Log("Export complete: " + Path.GetFullPath(exportPath));
+        }
+    }
+}
+
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # hello-world-unity-package
+
+
+
+# Reference
+
+`PackageExporter.cs`は、UniRxのものをお借りしています。
+
+https://github.com/neuecc/UniRx
+
+https://github.com/neuecc/UniRx/blob/master/Assets/Editor/PackageExporter.cs
